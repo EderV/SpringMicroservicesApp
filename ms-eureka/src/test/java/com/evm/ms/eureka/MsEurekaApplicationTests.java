33de8a82b0f08268e13bc871f19b3babd53d94d6
EderV <36447839+EderV@users.noreply.github.com>
2024-03-27 23:57:06 +0300

    First commit\n Added ms-config, ms-eureka and ms-gateway. All are configured

---
+++ ms-eureka/src/test/java/com/evm/ms/eureka/MsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evm.ms.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
