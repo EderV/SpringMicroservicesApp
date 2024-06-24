fca01d943bb25bdbe1a30b4a80d5afe6324f988a
EderV <evillar@nsign.tv>
2024-06-24 17:22:38 +0300

    Added ms-notifier Updated Spring Boot: 3.2.4 -> 3.2.7

---
+++ ms-notifier/src/test/java/com/evm/ms/notifier/MsNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evm.ms.notifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
